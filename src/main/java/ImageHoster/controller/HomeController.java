a4107a11cba9604f397b2e3e4b2e43a47e635717
adiJiyer <janardhananaditya@gmail.com>
2020-10-24 16:27:03 +0530

    Display Image

--- src/main/java/ImageHoster/controller/HomeController.java
+++ src/main/java/ImageHoster/controller/HomeController.java
@@ -1,5 +1,7 @@
 package ImageHoster.controller;
 
+import ImageHoster.model.Image;
+import ImageHoster.service.ImageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,8 +12,17 @@ import java.util.List;
 @Controller
 public class HomeController {
 
+    //Write the code to inject the ImageService bean
+    @Autowired
+    private ImageService imageService;
+
     @RequestMapping("/")
     public String getAllImages(Model model) {
+        //Call getAllImages() method in ImageService class to get the list of all images
+        //Add the list of images in the model with the key as "images"
+        List<Image> images = imageService.getAllImages();
+        model.addAttribute("images", images);
+
         return "index";
     }
 }
